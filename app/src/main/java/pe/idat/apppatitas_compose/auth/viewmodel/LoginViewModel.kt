a43d4cb395cfa7f610efed03af5893149ed689ac
ciclo6moviles2024idat <ciclo6moviles2024idat@gmail.com>
2024-07-22 19:36:51 -0500

    LoginViewModel V1.0

--- app/src/main/java/pe/idat/apppatitas_compose/auth/viewmodel/LoginViewModel.kt
+++ app/src/main/java/pe/idat/apppatitas_compose/auth/viewmodel/LoginViewModel.kt
@@ -1,4 +1,26 @@
 package pe.idat.apppatitas_compose.auth.viewmodel
 
-class LoginViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+
+    private val _usuario = MutableLiveData<String>()
+    val usuario: LiveData<String> = _usuario
+    private val _password = MutableLiveData<String>()
+    val password: LiveData<String> = _password
+    private var _botonLoginHabilitado = MutableLiveData<Boolean>()
+    var botonLoginHabilitado : LiveData<Boolean> = _botonLoginHabilitado
+
+    fun onValueChanged(usuario: String, password: String){
+        _usuario.value = usuario
+        _password.value = password
+        _botonLoginHabilitado.value = habilitarBotonLogin(usuario, password)
+    }
+    fun habilitarBotonLogin(usuario:String, password: String) = usuario.length > 2
+            && password.length > 2
 }
