62d1f837ceb72a228715552dd520fd64eba9caeb
ciclo6moviles2024idat <ciclo6moviles2024idat@gmail.com>
2024-07-22 19:25:49 -0500

    definición de rutas en el activity

--- app/src/main/java/pe/idat/apppatitas_compose/MainActivity.kt
+++ app/src/main/java/pe/idat/apppatitas_compose/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import pe.idat.apppatitas_compose.auth.view.loginScreen
 import pe.idat.apppatitas_compose.auth.view.registroScreen
+import pe.idat.apppatitas_compose.core.ruta.RutaPatitas
 import pe.idat.apppatitas_compose.ui.theme.ApppatitascomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +20,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ApppatitascomposeTheme {
-                loginScreen()
+                val navigation = rememberNavController()
+                NavHost(navController = navigation,
+                    startDestination = RutaPatitas.loginScreen.path,
+                    builder = {
+                        composable(RutaPatitas.loginScreen.path){
+                            loginScreen()
+                        }
+                        composable(RutaPatitas.registroScreen.path){
+                            registroScreen()
+                        }
+                    })
             }
         }
     }
