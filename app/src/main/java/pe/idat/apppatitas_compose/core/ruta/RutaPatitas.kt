3aa7864dbfe06e76606d49c0601808edd1404ac9
ciclo6moviles2024idat <ciclo6moviles2024idat@gmail.com>
2024-07-22 19:18:33 -0500

    agregando Hilt al proyecto y definición de rutas

--- app/src/main/java/pe/idat/apppatitas_compose/core/ruta/RutaPatitas.kt
+++ app/src/main/java/pe/idat/apppatitas_compose/core/ruta/RutaPatitas.kt
@@ -1,4 +1,7 @@
 package pe.idat.apppatitas_compose.core.ruta
 
-class RutaPatitas {
+sealed class RutaPatitas(val path: String) {
+    object loginScreen: RutaPatitas("loginScreen")
+    object registroScreen: RutaPatitas("registroScreen")
+    object homeScreen: RutaPatitas("homeScreen")
 }
